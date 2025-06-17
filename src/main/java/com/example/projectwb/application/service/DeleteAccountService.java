a7da67891dc806a88f57aab87e8c04b4a1de7724
sung1002 <lunesave@gmail.com>
2025-06-18 02:40:21 +0900

    계좌 삭제 예외처리 추가

--- src/main/java/com/example/projectwb/application/service/DeleteAccountService.java
+++ src/main/java/com/example/projectwb/application/service/DeleteAccountService.java
@@ -2,6 +2,9 @@ package com.example.projectwb.application.service;
 
 import com.example.projectwb.application.port.in.DeleteAccountUseCase;
 import com.example.projectwb.application.port.out.DeleteAccountPort;
+import com.example.projectwb.application.port.out.LoadAccountPort;
+import com.example.projectwb.domain.Account;
+import com.example.projectwb.domain.exception.AccountNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,9 +15,12 @@ import org.springframework.stereotype.Service;
 public class DeleteAccountService implements DeleteAccountUseCase {
 
     private final DeleteAccountPort deleteAccountPort;
+    private final LoadAccountPort loadAccountPort;
 
     @Override
     public void deleteAccount(Long accountId) {
-        deleteAccountPort.deleteAccount(accountId);
+        Account account = loadAccountPort.loadAccount(accountId)
+            .orElseThrow(() -> new AccountNotFoundException("삭제할 계좌를 찾을 수 없습니다: " + accountId));
+        deleteAccountPort.deleteAccount(account.getId());
     }
 }
